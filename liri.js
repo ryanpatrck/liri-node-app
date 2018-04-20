7436bad683fa1b3fb223e7b0e89a5da5629a3036
Ryan <Admin@MacBook-Pro.local>
2018-04-19 23:56:55 -0400

    liri

--- liri.js
+++ liri.js
@@ -1,12 +1,27 @@
-require("dotenv").config();
 
+
+//Grab data from dotenv file
+require("dotenv").config();
+var request= require('request')
 var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
-
 var spotify = new Spotify(keys.spotify);
 
+//Stored arguemtn's array
+var nodeArgv = process.argv;
+var command = process.argv[2];
+// movie or song
+var x = "";
 
+//attaches multiple word arguments
+for (var i=3; i<nodeArgv.length; i++){
+    if(i>3 && i<nodeArgv.length){
+      x = x + "+" + nodeArgv[i];
+    } else{
+      x = x + nodeArgv[i];
+    }
+  }
 var getMyTweets = function(){
     var client = new Twitter(keys.twitter);
 
@@ -23,20 +38,75 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 });
 
 }
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+var artistName = function(artist) {
+    return artist.name;
+}
+var spotifyMe = function(songName){
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+         console.log('Error occurred: ' + err);
+          return;
+        }
+
+        var songs = data.tracks.items;
+        for (var i=0; i<songs.length; i++){
+            console.log(i);
+            console.log('artist(s): ' + songs[i].artists.map(artistName));
+            console.log('song name: '+songs[i].name);
+            console.log('preview song: '+ songs[i].preview_url);
+            console.log('album: '+songs[i].album.name);
+            console.log('----------------------');
+        }
+       
+    
+      });
+
+}
+var MovieMe = function(movieName){
+    request('http://www.omdbapi.com/?apikey=trilogy&t=pulp+fiction&y=&plot=short&r=json', function (error, response, body){
+if (!error && response.statusCode == 200) {
+    
+    var jsonData = JSON.parse(body);
+
+    console.log('Title: ' +jsonData.Title);
+    console.log('Year: '+jsonData.Year);
+    console.log('Rated: '+jsonData.Rated);
+    console.log('IMDB Rating: ' + jsonData.imdbRating);
+    console.log('Language: ' + jsonData.Language);
+    console.log('Plot: '+ jsonData.Plot);
+    console.log('Actors: '+ jsonData.Actors);
+    console.log('Rotten tomatoes rating: '+ jsonData.tomatoRating);
+
     }
-   
-  console.log(data.tracks.items[0]); 
-  });
+})
+    
+}
+
+
+
+
 var picks = function(caseData, functionData){
     switch(caseData){
         case 'my-tweets' :
         getMyTweets();
-        break;
         default:
         console.log('LIRI does not know that');
+        break;
+        
+        case 'spotify-this-song':
+       if (x){ spotifyMe(functionData)
+       } else {
+           spotifyMe("The Sign");
+       }
+       break;
+
+       case 'movie-this':
+       if (x){ MovieMe(functionData)
+        } else {
+            movieMe("Mr. Nobody")
+        }
+
+       
     }
 }
 
